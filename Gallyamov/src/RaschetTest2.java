c14d78c99ffad2f51ae6f4120617dd96eacad402
RuslanG2 <floooyooo31@gmail.com>
2021-03-03 19:27:55 +0500

    Добавил тесты

---
+++ Gallyamov/src/RaschetTest2.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RaschetTest2 {
+
+	@Test
+	public void test() {
+		Raschet r = new Raschet();
+		
+		double osago = r.formula(1,1,1,1,1,1,1);
+		if(osago==1) {
+			assertTrue(true);
+		}else {
+			assertTrue(false);
+		}
+		
+	}
+
+}
